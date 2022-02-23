667ef4b57b9577f8fab328041570c173157e8982
ismafitria26 <2041720033@student.polinema.ac.id>
2022-02-23 15:14:13 +0700

    Jobsheet2(Praktikum2-2)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,9 +37,15 @@ Route::get('/articles/{id}', function ($id) {
 
 
 //PRAKTIKUM 2
+/*
 //nomor1
 Route::get('/', [PageController::class, 'index']);
-//nomor2
 Route::get('/about', [PageController::class, 'about']);
-//nomor3
 Route::get('/articles/{id}', [PageController::class, 'articles']);
+*/
+
+//nomor2
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
